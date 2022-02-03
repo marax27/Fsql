0bcd884bd4923635756a3171fbd2cab0819e6512
marax27 <kacpert314@gmail.com>
2022-02-03 16:51:48 +0100

    Debug/Release setup

--- Fsql.Cli/Program.cs
+++ Fsql.Cli/Program.cs
@@ -14,14 +14,48 @@ while (true)
     if (input is null || input.Equals("quit", StringComparison.OrdinalIgnoreCase))
         break;
 
+#if (DEBUG)
     var query = parser.Parse(input);
     var qe = new QueryEvaluation(access);
 
     var result = qe.Evaluate(query);
-    DisplayEvaluationResults(result);
+    DisplayQueryResults(result);
+#elif (RELEASE)
+    try
+    {
+        var query = parser.Parse(input);
+        var qe = new QueryEvaluation(access);
+
+        var result = qe.Evaluate(query);
+        DisplayQueryResults(result);
+    }
+    catch (ParserException exc)
+    {
+        PrintColor($"Parser exception: {exc.Message}", ConsoleColor.Yellow);
+        foreach (var error in exc.Errors ?? new List<string>())
+            PrintColor("    - " + error, ConsoleColor.Yellow);
+    }
+    catch (ApplicationException exc)
+    {
+        PrintColor($"Application exception: {exc.Message}", ConsoleColor.Yellow);
+        PrintColor(exc.StackTrace ?? "", ConsoleColor.Yellow);
+    }
+    catch (Exception exc)
+    {
+        PrintColor($"Unhandled exception: {exc.Message}", ConsoleColor.Red);
+        Console.WriteLine(exc.StackTrace);
+    }
+#endif
 }
 
-void DisplayEvaluationResults(QueryEvaluationResult evaluationResult)
+void PrintColor(string message, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(message);
+    Console.ResetColor();
+}
+
+void DisplayQueryResults(QueryEvaluationResult evaluationResult)
 {
     var attributeNames = evaluationResult.AttributeNames;
     Console.WriteLine(FormatRow(attributeNames));

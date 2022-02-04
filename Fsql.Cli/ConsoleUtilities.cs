33cf25b423eb8443d7d0cf2dae22b087fc5c7dbf
marax27 <kacpert314@gmail.com>
2022-02-04 23:45:22 +0100

    Add extra info to console output

---
+++ Fsql.Cli/ConsoleUtilities.cs
@@ -0,0 +1,11 @@
+﻿namespace Fsql.Cli;
+
+internal class ConsoleUtilities
+{
+    public static void PrintColor(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+}

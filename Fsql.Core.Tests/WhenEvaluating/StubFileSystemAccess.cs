58ba3c93478e3027bf30762ae89ebb16a239d591
marax27 <kacpert314@gmail.com>
2022-02-04 17:20:48 +0100

    Extract StubFileSystemAccess

---
+++ Fsql.Core.Tests/WhenEvaluating/StubFileSystemAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Fsql.Core.FileSystem;
+
+namespace Fsql.Core.Tests.WhenEvaluating;
+
+internal class StubFileSystemAccess : IFileSystemAccess
+{
+    private readonly IEnumerable<BaseFileSystemEntry> _entries;
+
+    public StubFileSystemAccess(IEnumerable<BaseFileSystemEntry> entries)
+    {
+        _entries = entries;
+    }
+
+    public IEnumerable<BaseFileSystemEntry> GetEntries(string directoryPath) => _entries;
+}
--- Fsql.Core.Tests/WhenEvaluating/WhenEvaluatingWithOrdering.cs
+++ Fsql.Core.Tests/WhenEvaluating/WhenEvaluatingWithOrdering.cs
@@ -14,7 +14,7 @@ public class WhenEvaluatingWithOrdering
     {
         var expectedNames = new[] { "aaa", "ADirectory", "azz", "BDirectory", "ZDirectory" };
         var givenQuery = new Query(new[] { "name" }, "./path", new(new[] { new OrderCondition("name", true) }));
-        var sut = new QueryEvaluation(new StubFileSystemAccess());
+        var sut = new QueryEvaluation(new StubFileSystemAccess(GivenEntries));
 
         var result = sut.Evaluate(givenQuery);
 
@@ -28,7 +28,7 @@ public class WhenEvaluatingWithOrdering
     {
         var expectedNames = new[] { "ZDirectory", "BDirectory", "azz", "ADirectory", "aaa" };
         var givenQuery = new Query(new[] { "name" }, "./path", new(new[] { new OrderCondition("name", false) }));
-        var sut = new QueryEvaluation(new StubFileSystemAccess());
+        var sut = new QueryEvaluation(new StubFileSystemAccess(GivenEntries));
 
         var result = sut.Evaluate(givenQuery);
 
@@ -42,7 +42,7 @@ public class WhenEvaluatingWithOrdering
     {
         var expectedNames = new[] { "azz", "aaa", "ZDirectory", "BDirectory", "ADirectory" };
         var givenQuery = new Query(new[] { "name", "size" }, "./path", new(new[] { new OrderCondition("size", true) }));
-        var sut = new QueryEvaluation(new StubFileSystemAccess());
+        var sut = new QueryEvaluation(new StubFileSystemAccess(GivenEntries));
 
         var result = sut.Evaluate(givenQuery);
 
@@ -56,7 +56,7 @@ public class WhenEvaluatingWithOrdering
     {
         var expectedNames = new[] { "ADirectory", "BDirectory", "ZDirectory", "aaa", "azz" };
         var givenQuery = new Query(new[] { "name", "size" }, "./path", new(new[] { new OrderCondition("size", false) }));
-        var sut = new QueryEvaluation(new StubFileSystemAccess());
+        var sut = new QueryEvaluation(new StubFileSystemAccess(GivenEntries));
 
         var result = sut.Evaluate(givenQuery);
 
@@ -64,19 +64,15 @@ public class WhenEvaluatingWithOrdering
         actualNames.Should().BeEquivalentTo(expectedNames,
             o => o.WithStrictOrdering());
     }
-}
 
-internal class StubFileSystemAccess : IFileSystemAccess
-{
-    public const string GivenPath = "./path";
+    private static string GivenPath => "./path";
 
-    public IEnumerable<BaseFileSystemEntry> GetEntries(string directoryPath)
-        => new List<BaseFileSystemEntry>
-        {
-            new FakeFileSystemEntry($"{GivenPath}/aaa", FileSystemEntryType.File, 500),
-            new FakeFileSystemEntry($"{GivenPath}/azz", FileSystemEntryType.File, 128),
-            new FakeFileSystemEntry($"{GivenPath}/ZDirectory", FileSystemEntryType.Directory, 0),
-            new FakeFileSystemEntry($"{GivenPath}/BDirectory", FileSystemEntryType.Directory, 0),
-            new FakeFileSystemEntry($"{GivenPath}/ADirectory", FileSystemEntryType.Directory, 0),
-        };
+    private static IEnumerable<BaseFileSystemEntry> GivenEntries => new List<BaseFileSystemEntry>
+    {
+        new FakeFileSystemEntry($"{GivenPath}/aaa", FileSystemEntryType.File, 500),
+        new FakeFileSystemEntry($"{GivenPath}/azz", FileSystemEntryType.File, 128),
+        new FakeFileSystemEntry($"{GivenPath}/ZDirectory", FileSystemEntryType.Directory, 0),
+        new FakeFileSystemEntry($"{GivenPath}/BDirectory", FileSystemEntryType.Directory, 0),
+        new FakeFileSystemEntry($"{GivenPath}/ADirectory", FileSystemEntryType.Directory, 0),
+    };
 }
